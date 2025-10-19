3f035a2fe21ecd23dab8d1b3d5c2ff23a0bfdf7b
MaddieP <232483035+maddiepdl@users.noreply.github.com>
2025-10-19 00:18:56 -0500

    update script.js for accordion functions and add aria

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 // selects all questions
-const questions = document.querySelectorAll(".faq-question");
+const questions = document.querySelectorAll('.faq-question');
 
-// loop through each button
-questions.forEach((btn) => {
-  btn.addEventListener("click", () => toggleFAQ(btn));
+// loop through buttons and add click event
+questions.forEach(button => {
+  button.addEventListener('click', () => {
+    const expanded = button.getAttribute('aria-expanded') === 'true';
 
-  // keydown event for enter or space
-  btn.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" || e.key === " ") {
-      e.preventDefault(); 
-      toggleFAQ(btn);     
+    // close answers and reset aria
+    questions.forEach(btn => {
+      btn.setAttribute('aria-expanded', 'false');
+      document.getElementById(btn.getAttribute('aria-controls')).hidden = true;
+    });
+
+    // expand clicked one to show answer and update aria
+    if (!expanded) {
+      button.setAttribute('aria-expanded', 'true');
+      document.getElementById(button.getAttribute('aria-controls')).hidden = false;
     }
   });
-});
-
-// open/close function for accordion 
-function toggleFAQ(button) {
-  const expanded = button.getAttribute("aria-expanded") === "true";
-
-  const answer = document.getElementById(button.getAttribute("aria-controls"));
-
-  questions.forEach((q) => {
-    q.setAttribute("aria-expanded", "false");
-    document.getElementById(q.getAttribute("aria-controls")).hidden = true; 
-  });
-
-  if (!expanded) {
-    button.setAttribute("aria-expanded", "true"); 
-    answer.hidden = false; // shows answer
-  }
-}
+})
